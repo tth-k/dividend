4201ed5743ac43d093e310dff96303107543e9be
김소연 <thdus2282@gamil.com>
2023-11-03 21:56:59 +0900

    추가

---
+++ src/main/java/com/dayone/service/MemberService.java
@@ -0,0 +1,48 @@
+package com.dayone.service;
+
+import com.dayone.exception.impl.AlreadyExistsUserException;
+import com.dayone.model.Auth;
+import com.dayone.persist.entity.MemberEntity;
+import com.dayone.persist.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class MemberService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.memberRepository.findByUsername(username)
+                                .orElseThrow(() -> new UsernameNotFoundException("couldn't find user ->" + username));
+    }
+
+    public MemberEntity register(Auth.SignUp member){
+        boolean exists = this.memberRepository.existsByUsername(member.getUsername());
+        if (exists){
+            throw new AlreadyExistsUserException();
+        }
+
+        member.setPassword(this.passwordEncoder.encode(member.getPassword()));
+        var result =  this.memberRepository.save(member.toEntity());
+        return result;
+    }
+
+    public MemberEntity authenticate(Auth.SignIn member) {
+        var user = this.memberRepository.findByUsername(member.getUserName())
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 ID입니다."));
+        if (!this.passwordEncoder.matches(member.getPassword(), user.getPassword())){
+            throw new RuntimeException("비밀번호가 일치하지 않습니다");
+        }
+        return user;
+    }
+}
